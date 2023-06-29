b056792e5fa572d7ebd6607d95b465a788ac0e8a
lGutierrezF <l.gutierrez@students.salle.url.edu>
2023-06-29 20:48:11 +0200

    add: the rest of the required functionalities.

--- src/main/scala/core/core.scala
+++ src/main/scala/core/core.scala
@@ -1,20 +1,12 @@
 package core
-  case class PostalCode private (zipCode: String)
+
+import java.time.OffsetDateTime
+
+case class PostalCode private (zipCode: String)
   object PostalCode {
     def createPostalCode(postalCode: String): Option[PostalCode] = {
       if (postalCode.matches("\\d{5}"))
         Option(new PostalCode(postalCode))
-      else
-        None
-    }
-  }
-
-  //TODO: PostalCodeCollection should have private attributes, fix createPackage so it doesn't directly access the postalCodes attribute
-  case class Package private (weight: Double, postalCode: PostalCode)
-  object Package {
-    def createPackage(weight: Double, postalCode: PostalCode, validPostalCodeCollection: PostalCodeCollection): Option[Package] = {
-      if (weight > 0 && validPostalCodeCollection.postalCodes.contains(postalCode))
-        Option(new Package(weight, postalCode))
       else
         None
     }
@@ -46,12 +38,143 @@ package core
         .foldLeft(Option(PostalCodeCollection.emptyPostalCodeCollection))
         ((postalCodeCollection, postalCode) => PostalCodeCollection.addPostalCode(postalCode, postalCodeCollection))
     }
-    
-    //TODO: Create Person class, traits and companion object
+  }
+
+  case class Package private(id:String, weight: Double, postalCode: PostalCode){
+    override def equals(obj: Any): Boolean = {
+      obj match {
+        case packageToCompare: Package => packageToCompare.id == this.id
+        case _ => false
+      }
+    }
+  }
+
+  object Package {
+    def createPackage(id: String, weight: Double, postalCode: PostalCode, validPostalCodeCollection: PostalCodeCollection): Option[Package] = {
+      if (weight > 0 && validPostalCodeCollection.postalCodes.contains(postalCode))
+        Option(new Package(id, weight, postalCode))
+      else
+        None
+    }
+  }
+
+  case class PackageCollection private (packages: Set[Package])
+  trait PackageCollectionInterface {
+    def emptyPackageCollection: PackageCollection
+    def addPackage(packageToAdd: Option[Package], packageCollection: Option[PackageCollection]): Option[PackageCollection]
+    def createPackageCollection(packages: Seq[Option[Package]]): Option[PackageCollection]
+  }
+  object PackageCollection extends PackageCollectionInterface {
+    def emptyPackageCollection: PackageCollection = new PackageCollection(Set.empty)
+    def addPackage(packageToAdd: Option[Package], packageCollection: Option[PackageCollection]): Option[PackageCollection] = {
+      if (
+        packageToAdd.isEmpty
+        || packageCollection.isEmpty
+        || packageCollection.get.packages.contains(packageToAdd.get))
+        None
+      else
+        Option(new PackageCollection(packageCollection.get.packages + packageToAdd.get))
+    }
+    def createPackageCollection(packages: Seq[Option[Package]]): Option[PackageCollection] = {
+      packages
+        .foldLeft(Option(PackageCollection.emptyPackageCollection))
+        ((packageCollection, packageToAdd) => PackageCollection.addPackage(packageToAdd, packageCollection))
+    }
+  }
+  case class Person private (name: String)
+  object Person {
+    def createPerson(name: String): Option[Person] = {
+      if (name.matches("[a-zA-Z]+"))
+        Option(new Person(name))
+      else
+        None
+    }
+  }
+  case class DeliveryRoute (packages: Seq[Package], postalCode: PostalCode, deliveryPerson: Person)
+
+  case class DeliveryPlanner (packages: PackageCollection,
+                                    postalCodes: PostalCodeCollection,
+                                    deliveryPersonLight: Person,
+                                    deliveryPersonHeavy: Person)
+  object DeliveryPlanner {
+    private val _heavyThreshold = 2.0
+
+    def createDeliveryRoutes(deliveryPlanner: DeliveryPlanner): Seq[DeliveryRoute] = {
+      val packages = deliveryPlanner.packages.packages.toSeq
+      val postalCodes = deliveryPlanner.postalCodes.postalCodes.toSeq
+      val deliveryPersonLight = deliveryPlanner.deliveryPersonLight
+      val deliveryPersonHeavy = deliveryPlanner.deliveryPersonHeavy
+
+      val lightPackagesToDeliver = packages
+        .filter(packageToDeliver => packageToDeliver.weight <= _heavyThreshold)
+
+      val heavyPackagesToDeliver = packages
+        .filter(packageToDeliver => packageToDeliver.weight > _heavyThreshold)
+
+      val lightDeliveryRoutes = createDeliveryRoute(postalCodes, deliveryPersonLight, lightPackagesToDeliver)
+
+      val heavyDeliveryRoutes = createDeliveryRoute(postalCodes, deliveryPersonHeavy, heavyPackagesToDeliver)
+      val deliveryRoutes = lightDeliveryRoutes ++ heavyDeliveryRoutes
+      deliveryRoutes.filter(_.packages.nonEmpty)
+    }
+
+    private def createDeliveryRoute(postalCodes: Seq[PostalCode], deliveryPerson: Person, packagesToDeliver: Seq[Package]) = {
+      val deliveryRoutes = for postalCode <- postalCodes yield
+        val packagesByPostalCode = packagesToDeliver
+          .filter(_.postalCode == postalCode)
+        DeliveryRoute(packagesByPostalCode, postalCode, deliveryPerson)
+      deliveryRoutes.toSeq
+    }
+  }
+
+  case class Signature ()
+  case class DeliveryReceipt private(packageId:String, dni: String, signature: Signature, date: OffsetDateTime, deliveryCode: String)
+  object DeliveryReceipt {
+    def apply(packageId:String, dni: String, date: OffsetDateTime, deliveryCode: String): DeliveryReceipt = {
+      new DeliveryReceipt(packageId, dni,Signature(), date, deliveryCode)
+    }
+    def apply(packageId:String,signature: Signature, date: OffsetDateTime, deliveryCode: String): DeliveryReceipt = {
+      new DeliveryReceipt(packageId, "", signature, date, deliveryCode)
+    }
   }
 
 
 
+// Dependency injection was used to keep DeliveryRunner.run function pure
+  trait GetCurrentTime(){
+    def now():OffsetDateTime
+  }
 
+  trait ObtainCustomerDni(){
+    def get(packageId:String):String
+  }
 
+  trait ObtainCustomerPhone(){
+      def get():Integer
+    }
 
+  trait SMSSender(){
+    def send(phoneNumber: Integer, message:String):Unit
+  }
+
+  trait LogRegister(){
+    def logError(message:String):Unit
+  }
+
+  object DeliveryRunner:
+    def run(deliveryRoute: DeliveryRoute, getCurrentTime:GetCurrentTime, obtainCustomerDni:ObtainCustomerDni,obtainCustomerPhone:ObtainCustomerPhone, SMSSender: SMSSender, logRegister: LogRegister): Seq[DeliveryReceipt] = {
+      val packages = deliveryRoute.packages
+      val postalCode = deliveryRoute.postalCode
+      val deliveryPerson = deliveryRoute.deliveryPerson
+
+      val deliveryReceipts = for packageToDeliver <- packages yield
+        val packageId = packageToDeliver.id
+        val deliveryReceipt = DeliveryReceipt(packageToDeliver.id, obtainCustomerDni.get(packageToDeliver.id),getCurrentTime.now(), packageId)
+        try {
+          SMSSender.send(obtainCustomerPhone.get(),s"The package $packageId has been delivered")
+        } catch {
+          case e => logRegister.logError(e.getMessage)
+        }
+        deliveryReceipt
+      deliveryReceipts.toSeq
+    }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,16 +1,116 @@
 import core.*
+import java.time.OffsetDateTime
+
 @main
 def main(): Unit = {
+
+  val postalCodeCollection = getPostalCodeCollectionByTerminalInput
+  val packageCollection = getPackageCollectionByTerminalInput(postalCodeCollection)
+  val lightDeliveryPerson = getPersonByTerminalInput("light")
+  val heavyDeliveryPerson = getPersonByTerminalInput("heavy")
+  val deliveryRoutes = DeliveryPlanner.createDeliveryRoutes(DeliveryPlanner(packageCollection, postalCodeCollection,
+    lightDeliveryPerson, heavyDeliveryPerson))
+  val deliveredPackageInformation = for deliveryRoute <- deliveryRoutes yield
+    DeliveryRunner.run(deliveryRoute, JavaGetCurrentTime, ConsoleObtainCustomerDni, ConsoleObtainCustomerPhone,
+      DummySMSSender, ConsoleLogRegister)
+}
+
+def getPostalCodeCollectionByTerminalInput = {
   println("Please input the postal codes separated by spaces.")
   val postalCodes = scala.io.StdIn.readLine().split(" ").toList
   val postalCodeSeq = postalCodes
     .map(PostalCode.createPostalCode(_))
-  val postalCodeCollection = PostalCodeCollection.createPostalCodeCollection(postalCodeSeq)
-  //TODO: Add packages
+  var postalCodeCollection = PostalCodeCollection.createPostalCodeCollection(postalCodeSeq)
+  while postalCodeCollection == None do
+    println("The provided inputs are invalid.")
+    println("Please input the postal codes separated by spaces.")
+    val postalCodesRetry = scala.io.StdIn.readLine().split(" ").toList
+    val postalCodeSeqRetry = postalCodesRetry
+      .map(PostalCode.createPostalCode(_))
+    postalCodeCollection = PostalCodeCollection.createPostalCodeCollection(postalCodeSeqRetry)
+  postalCodeCollection.get
+}
+def getPackageCollectionByTerminalInput(validPostalCodeCollection: PostalCodeCollection) = {
+  println("How many packages do you want to introduce?")
+  val numberOfPackages = scala.io.StdIn.readLine().toInt
+  val packages = (1 to numberOfPackages).map(_ => getPackageByTerminalInput(validPostalCodeCollection))
 
-  //TODO: Start route of person
+  var packageCollection = PackageCollection.createPackageCollection(packages)
+  while packageCollection.isEmpty do
+    println("The provided package collection is invalid.")
+    packageCollection = PackageCollection.createPackageCollection(packages)
+  packageCollection.get
+}
+def getPackageByTerminalInput(validPostalCodeCollection: PostalCodeCollection): Option[Package] = {
+  println("Please input the packages id.")
+  val id = scala.io.StdIn.readLine()
+  println("Please input the packages weight.")
+  val weight = scala.io.StdIn.readLine().toDouble
+  println("Please input the packages postal code.")
+  val postalCodeString = scala.io.StdIn.readLine()
+  val postalCode = PostalCode.createPostalCode(postalCodeString)
+  val package_object = if postalCode.isEmpty then
+    None
+  else
+    Package.createPackage(id, weight, postalCode.get, validPostalCodeCollection)
 
-    //TODO: Delivery package
+  if package_object.isEmpty then
+    println("The provided inputs are invalid so the package were not created.")
+    getPackageByTerminalInput(validPostalCodeCollection)
+  else
+    package_object
+  end if
+}
 
-  println(postalCodeCollection)
+def getPersonByTerminalInput(personType: String): Person = {
+  println(s"Please input the $personType delivery person's name.")
+  val name = scala.io.StdIn.readLine()
+  val person = Person.createPerson(name)
+  if person.isEmpty then
+    println("The provided inputs are invalid so the person were not created.")
+    getPersonByTerminalInput(personType)
+  else
+    person.get
+}
+
+object ConsoleObtainCustomerDni extends ObtainCustomerDni {
+  def get(packageId: String): String = {
+    println("Please input the DNI of the package receiver.")
+    val id = scala.io.StdIn.readLine()
+    if id.isEmpty then
+      println("The provided inputs are invalid so the package were not created.")
+      get(packageId)
+    else
+      id
+    end if
+  }
+}
+
+object ConsoleObtainCustomerPhone extends ObtainCustomerPhone {
+  def get(): Integer = {
+    println("Please input the phone number of the package receiver.")
+    val id = scala.io.StdIn.readLine()
+    if id.isEmpty then
+      println("The provided inputs are invalid so the package were not created.")
+      get()
+    else
+      id.toInt
+    end if
+  }
+}
+
+object JavaGetCurrentTime extends GetCurrentTime {
+  def now(): OffsetDateTime = OffsetDateTime.now()
+}
+
+object DummySMSSender extends SMSSender {
+  def send(phoneNumber: Integer, message: String): Unit = {
+    println(s"Sending message $message to $phoneNumber")
+  }
+}
+
+object ConsoleLogRegister extends LogRegister() {
+  def logError(message: String): Unit = {
+    println(s"An error has ocurred: $message")
+  }
 }
